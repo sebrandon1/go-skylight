121e56d614763bc9c5b5c2d392bb8493362f2ae7
MoneyKalle <monetoskarl@gmail.com>
2026-08-13 16:38:58 +0000

    fix: dereference bounty at call site and use value type in switch
    Address review feedback: printOutput(*bounty) at the call site
    removes the nil-pointer dereference risk in the table-output
    switch. The switch case is now a value type (lib.Bounty),
    consistent with the rest of the switch.

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -209,8 +209,8 @@ func printTableOutput(data any) bool {
 		printCalendarWeekTable(v)
 	case []lib.Bounty:
 		printBountiesTable(v)
-	case *lib.Bounty:
-		printBountiesTable([]lib.Bounty{*v})
+	case lib.Bounty:
+		printBountiesTable([]lib.Bounty{v})
 	default:
 		return printTableOutputMore(data)
 	}
